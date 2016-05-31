d28b069c430ef1b758a59871fb94ba4b8969a69e
Nithin Vommi <nvommi@hpc-login-1-2.local>
2016-05-30 18:20:51 -0700

    Version 1

--- src/silhouette.sh
+++ src/silhouette.sh
@@ -17,4 +17,4 @@ module load boost/1.57.0
 module load openmpi-1.8.3/gcc-4.9.2
 
 # Run the program 
-mpirun -np 4 -npernode 1 ./silhouette_mpi_sr
+mpirun -quiet -np 4 -npernode 1 ./silhouette_mpi_sr
